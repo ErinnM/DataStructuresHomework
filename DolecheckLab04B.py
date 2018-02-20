2f6553c1b2d86c336dab29a551e0e16e8b90e550
ErinnM <33555351+ErinnM@users.noreply.github.com>
2018-02-19 18:12:33 -0600

    Update DolecheckLab04B.py

--- DolecheckLab04B.py
+++ DolecheckLab04B.py
@@ -1,14 +1,12 @@
-#Lab 04, Part B
+#Lab 04, Parts B and C
 #Erin Dolecheck
 
-#Without Recursion
-
 import time
-
 n=int(input("Enter which Fibonacci number you would like to calculate: "))
+
+#Without Recursion
 start=time.clock()
-x=1
-y=1
+x=y=1
 if n<2:
     print("1")
 else:
@@ -16,29 +14,44 @@ else:
         fib=x+y
         x=y
         y=fib
-    print(fib)
+    print(x)
 end=time.clock()
-wor=end-start
+wor=end-start       #wor-without recursion
 print(wor, "seconds without recursion.")
 
 #With Recursion
-
-def fib(n):
+def fibrecursion(n):
     if n<2:
         return(1)
     else:
-        return fib(n-1)+fib(n-2)
+        return fibrecursion(n-1)+fibrecursion(n-2)
     
 start2=time.clock()
-print(fib(n-1))
+print(fibrecursion(n-1))
 end2=time.clock()
-wr=end2-start2
+wr=end2-start2      #wr-with recursion
 print(wr, "seconds with recursion.")
 
-if wor<wr:
-    print('Without recursion is faster.')
-else:
-    print('With recursion is faster.')
-
 #Dynamic Programming 
-#I've been researching what this means, but I still don't completely understand
+key=[1,1]
+for i in range(n):
+    key.append(0)
+def fibdynamic(n):
+    if key[n]==0:
+        key[n]=fibdynamic(n-1)+fibdynamic(n-2)
+    return key[n]
+
+start3=time.clock()
+print(fibdynamic(n-1))
+end3=time.clock()
+wdp=end3-start3     #wdp-with dynamic programming
+print(wdp, "seconds with dynamic programming.")
+
+#Which is fastest?
+if wor<wr and wor<wdp:
+    print('Without recursion is fastest.')
+if wr<wor and wr<wdp:
+    print('Recursion is fastest.')
+else:
+    print("Dynamic programming is fastest.")
+
